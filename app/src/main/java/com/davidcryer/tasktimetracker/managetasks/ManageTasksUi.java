582e7ab04bae76f76de6b7047a180e91452f44e9
David Cryer <davecryer.dc@gmail.com>
2017-10-13 17:31:15 +0100

    Add method to ManageTasksUi to show stories

--- app/src/main/java/com/davidcryer/tasktimetracker/managetasks/ManageTasksUi.java
+++ app/src/main/java/com/davidcryer/tasktimetracker/managetasks/ManageTasksUi.java
@@ -1,6 +1,9 @@
 package com.davidcryer.tasktimetracker.managetasks;
 
+import java.util.List;
+
 public interface ManageTasksUi {
+    void showStories(List<UiStory> stories);
 
     interface Listener {
 
