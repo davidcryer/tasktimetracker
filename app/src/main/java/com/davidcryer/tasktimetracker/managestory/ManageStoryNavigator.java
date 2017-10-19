dade8d9752d7d08258c6904d35b9f3410a210edc
David Cryer <davecryer.dc@gmail.com>
2017-10-19 19:05:08 +0100

    Remove ManageStory functionality

--- app/src/main/java/com/davidcryer/tasktimetracker/managestory/ManageStoryNavigator.java
+++
@@ -1,7 +0,0 @@
-package com.davidcryer.tasktimetracker.managestory;
-
-import com.davidcryer.tasktimetracker.managetask.ManageTaskIntentModel;
-
-public interface ManageStoryNavigator {
-    void toManageTaskScreen(ManageTaskIntentModel intentModel);
-}
