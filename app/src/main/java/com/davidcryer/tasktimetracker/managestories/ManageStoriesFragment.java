4f04cfe0e0ee9bedd3151667cf75a31f46524a07
David Cryer <davecryer.dc@gmail.com>
2017-10-27 21:01:41 +0100

    Fix recyclerview not showing items

--- app/src/main/java/com/davidcryer/tasktimetracker/managestories/ManageStoriesFragment.java
+++ app/src/main/java/com/davidcryer/tasktimetracker/managestories/ManageStoriesFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,7 +46,9 @@ public class ManageStoriesFragment extends UiFragment<ManageStoriesUi.Listener, 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_manage_stories, container, false);
-        ((RecyclerView) view.findViewById(R.id.stories)).setAdapter(storiesAdapter);
+        final RecyclerView recyclerView = view.findViewById(R.id.stories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(storiesAdapter);
         return view;
     }
 
