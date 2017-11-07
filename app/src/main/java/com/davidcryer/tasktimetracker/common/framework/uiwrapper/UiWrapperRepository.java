28ef8454da1bac3518baa789840fa079e9c48e42
David Cryer <davecryer.dc@gmail.com>
2017-11-07 23:23:37 +0000

    fix variable name

--- app/src/main/java/com/davidcryer/tasktimetracker/common/framework/uiwrapper/UiWrapperRepository.java
+++ app/src/main/java/com/davidcryer/tasktimetracker/common/framework/uiwrapper/UiWrapperRepository.java
@@ -14,14 +14,14 @@ import java.util.Map;
 
 public class UiWrapperRepository {
     private final UiWrapperFactory uiWrapperFactory;
-    private final Map<String, UiWrapper<ManageStoriesUi, ManageStoriesUi.Listener, ManageStoriesUiModel>> manageTasksUiWrapperMap = new HashMap<>();
+    private final Map<String, UiWrapper<ManageStoriesUi, ManageStoriesUi.Listener, ManageStoriesUiModel>> manageStoriesUiWrapperMap = new HashMap<>();
 
     public UiWrapperRepository(final UiWrapperFactory uiWrapperFactory) {
         this.uiWrapperFactory = uiWrapperFactory;
     }
 
     public ManageStoriesUi.Listener bind(final ManageStoriesUi ui, final UiBinder binder) {
-        return binder.bind(ui, manageTasksUiWrapperMap, new UiBinder.UiWrapperProvider<ManageStoriesUi, ManageStoriesUi.Listener, ManageStoriesUiModel>() {
+        return binder.bind(ui, manageStoriesUiWrapperMap, new UiBinder.UiWrapperProvider<ManageStoriesUi, ManageStoriesUi.Listener, ManageStoriesUiModel>() {
             @NonNull
             @Override
             public UiWrapper<ManageStoriesUi, ManageStoriesUi.Listener, ManageStoriesUiModel> uiWrapper(Bundle savedInstanceState) {
@@ -31,6 +31,6 @@ public class UiWrapperRepository {
     }
 
     public void unbind(final ManageStoriesUi ui, final UiUnbinder unbinder) {
-        unbinder.unbind(manageTasksUiWrapperMap);
+        unbinder.unbind(manageStoriesUiWrapperMap);
     }
 }
