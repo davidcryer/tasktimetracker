b766389384cdd878cb7c21dd864a0a7624818afc
David Cryer <davecryer.dc@gmail.com>
2017-11-09 23:57:52 +0000

    give manage task fragment view with solid background

--- app/src/main/java/com/davidcryer/tasktimetracker/managetask/ManageTaskFragment.java
+++ app/src/main/java/com/davidcryer/tasktimetracker/managetask/ManageTaskFragment.java
@@ -2,10 +2,15 @@ package com.davidcryer.tasktimetracker.managetask;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.davidc.uiwrapper.UiBinder;
 import com.davidc.uiwrapper.UiFragment;
 import com.davidc.uiwrapper.UiUnbinder;
+import com.davidcryer.tasktimetracker.R;
 
 public class ManageTaskFragment extends UiFragment {
     private final static String ARGS_INTENT_MODEL = "intent model";
@@ -16,6 +21,12 @@ public class ManageTaskFragment extends UiFragment {
         args.putParcelable(ARGS_INTENT_MODEL, intentModel);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_manage_task, container, false);
     }
 
     @Override
