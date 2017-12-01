b9cb0e33d32b6343215352c55bd8c7324ba0573b
David Cryer <davecryer.dc@gmail.com>
2017-12-01 22:53:36 +0000

    remove unnecessary code

--- app/src/main/java/com/davidcryer/tasktimetracker/managecategories/TaskLayout.java
+++ app/src/main/java/com/davidcryer/tasktimetracker/managecategories/TaskLayout.java
@@ -11,13 +11,11 @@ import com.davidcryer.tasktimetracker.R;
 
 public class TaskLayout extends LinearLayout {
     private final TextView titleView;
-    private final View clicker;
 
     public TaskLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         inflate(context, R.layout.layout_task, this);
         titleView = findViewById(R.id.title);
-        clicker = this;
     }
 
     void task(final UiTask task) {
@@ -26,14 +24,5 @@ public class TaskLayout extends LinearLayout {
 
     private void title(final String title) {
         titleView.setText(title);
-    }
-
-    @Override
-    public void setOnClickListener(@Nullable OnClickListener l) {
-        if (clicker == this) {
-            super.setOnClickListener(l);
-        } else {
-            clicker.setOnClickListener(l);
-        }
     }
 }
