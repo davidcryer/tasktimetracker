a89e57a83f5fdb064820309078aa50a3add17bfe
David Cryer <davecryer.dc@gmail.com>
2018-05-21 20:47:33 +0100

    add previous files that were moved - git deleted old ones and didn't re-add

---
+++ app/src/main/java/com/davidcryer/tasktimetracker/common/domain/exceptions/CategoryArgResults.java
@@ -0,0 +1,70 @@
+package com.davidcryer.tasktimetracker.common.domain.exceptions;
+
+import com.davidcryer.argrules.multiarg.BrokenRulesException;
+import com.davidcryer.argrules.multiarg.Result;
+import com.davidcryer.argrules.multiarg.Results;
+
+public class CategoryArgResults extends Results<CategoryArgResults.Exception> {
+    private final Result id;
+    private final Result title;
+
+    private CategoryArgResults(Result id, Result title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public boolean hasTitleError() {
+        return title != null && !title.passed();
+    }
+
+    public Result id() {
+        return id;
+    }
+
+    public Result title() {
+        return title;
+    }
+
+    @Override
+    protected Result[] asArray() {
+        return new Result[] {id(), title()};
+    }
+
+    @Override
+    protected Exception exception() {
+        return new Exception(this);
+    }
+
+    static class Builder implements Results.Builder<CategoryArgResults> {
+        private Result id;
+        private Result title;
+
+        Builder id(final Result id) {
+            this.id = id;
+            return this;
+        }
+
+        Builder title(final Result title) {
+            this.title = title;
+            return this;
+        }
+
+        @Override
+        public CategoryArgResults results() {
+            return new CategoryArgResults(id, title);
+        }
+    }
+
+    public static class Exception extends BrokenRulesException {
+        private final CategoryArgResults results;
+
+        private Exception(CategoryArgResults results) {
+            super(results);
+            this.results = results;
+        }
+
+        public CategoryArgResults results() {
+            return results;
+        }
+    }
+}
---
+++ app/src/main/java/com/davidcryer/tasktimetracker/common/domain/exceptions/TaskArgResults.java
@@ -0,0 +1,81 @@
+package com.davidcryer.tasktimetracker.common.domain.exceptions;
+
+import com.davidcryer.argrules.multiarg.BrokenRulesException;
+import com.davidcryer.argrules.multiarg.Result;
+import com.davidcryer.argrules.multiarg.Results;
+
+public class TaskArgResults extends Results<TaskArgResults.Exception> {
+    private final Result id;
+    private final Result title;
+    private final Result ongoingSession;
+
+    private TaskArgResults(Result id, Result title, Result ongoingSession) {
+        this.id = id;
+        this.title = title;
+        this.ongoingSession = ongoingSession;
+    }
+
+    public boolean hasTitleError() {
+        return title != null && !title.passed();
+    }
+
+    public Result id() {
+        return id;
+    }
+
+    public Result title() {
+        return title;
+    }
+
+    public Result ongoingSession() {
+        return ongoingSession;
+    }
+
+    @Override
+    protected Result[] asArray() {
+        return new Result[] {id(), title(), ongoingSession()};
+    }
+
+    @Override
+    protected Exception exception() {
+        return new Exception(this);
+    }
+
+    static class Builder {
+        private Result id;
+        private Result title;
+        private Result ongoingSession;
+
+        Builder id(final Result id) {
+            this.id = id;
+            return this;
+        }
+
+        Builder title(final Result title) {
+            this.title = title;
+            return this;
+        }
+
+        Builder ongoingSession(final Result ongoingSession) {
+            this.ongoingSession = ongoingSession;
+            return this;
+        }
+
+        TaskArgResults create() {
+            return new TaskArgResults(id, title, ongoingSession);
+        }
+    }
+
+    public static class Exception extends BrokenRulesException {
+        private final TaskArgResults results;
+
+        private Exception(TaskArgResults results) {
+            super(results);
+            this.results = results;
+        }
+
+        public TaskArgResults results() {
+            return results;
+        }
+    }
+}
